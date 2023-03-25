c0e87f500b2c3b926e2f10c6657f9a3aedc26712
Long Pollehn <long.pollehn@gmail.com>
2023-03-25 10:30:26 +0100

    A bit slow, but it works

--- main.cpp
+++ main.cpp
@@ -1,12 +1,50 @@
 #include <ogdf/basic/Graph.h>
 
-ogdf::List<ogdf::AdjElement*> vecToList(const std::vector<ogdf::AdjElement*>& vec) {
-    ogdf::List<ogdf::AdjElement*> list;
-    for(int i = 0; i < list.size(); i++) {
-        list.pushBack(vec[i]);
+ogdf::List<ogdf::AdjElement *> vecToList(const std::vector<ogdf::AdjElement *> &vec) {
+    ogdf::List<ogdf::AdjElement *> list;
+    for (auto i : vec) {
+        list.pushBack(i);
     }
     return list;
 }
+
+class EnumerateEmbedding {
+    int numberOfNodes;
+    std::vector<ogdf::NodeElement *> nodes;
+    std::vector<std::vector<ogdf::AdjElement *>> rotation_system;
+    ogdf::Graph &m_G;
+    int embedding = 0;
+    std::function<void()> m_callback;
+public:
+    explicit EnumerateEmbedding(ogdf::Graph &G, const std::function<void()> &callback) : m_G(G), m_callback(callback) {
+        numberOfNodes = m_G.numberOfNodes();
+        rotation_system = std::vector<std::vector<ogdf::AdjElement *>>(numberOfNodes);
+        for (auto v: m_G.nodes) {
+            nodes.push_back(v);
+            for (auto adj: v->adjEntries) {
+                rotation_system[v->index()].push_back(adj);
+            }
+            std::sort(rotation_system[v->index()].begin(), rotation_system[v->index()].end());
+        }
+    }
+
+    int enumerate(int node = 0) {
+        if (node == 0) {
+            embedding = 0;
+        }
+        do {
+            m_G.sort(nodes[node], vecToList(rotation_system[node]));
+            if (node == numberOfNodes - 1) {
+                m_callback();
+                embedding++;
+            } else if (node < numberOfNodes - 1) {
+                enumerate(node + 1);
+            }
+        } while (std::next_permutation(rotation_system[node].begin(), rotation_system[node].end()));
+
+        return embedding;
+    }
+};
 
 int main() {
     ogdf::Graph graph;
@@ -21,32 +59,8 @@ int main() {
     graph.newEdge(v1, v3);
     graph.newEdge(v2, v4);
 
-    std::vector<std::vector<ogdf::AdjElement*>> rotation_system(4);
-    for (auto v: {v1, v2, v3, v4}) {
-        std::cout << "node: " << v->index() << std::endl;
-        for (auto adj: v->adjEntries) {
-            rotation_system[v->index()].push_back(adj);
-            std::cout << "\t" << v->index() << "->" << adj->theEdge()->opposite(v)->index() << std::endl;
-        }
-        std::sort(rotation_system[v->index()].begin(), rotation_system[v->index()].end());
-    }
-
-    auto embedding = 0;
-    do {
-        graph.sort(v1, vecToList(rotation_system[v1->index()]));
-        do {
-            graph.sort(v2, vecToList(rotation_system[v2->index()]));
-            do {
-                graph.sort(v3, vecToList(rotation_system[v3->index()]));
-                do {
-                    graph.sort(v4, vecToList(rotation_system[v4->index()]));
-                    std::cout << "Genus: " << graph.genus() << std::endl;
-                    embedding++;
-                } while (std::next_permutation(rotation_system[v4->index()].begin(), rotation_system[v4->index()].end()));
-            } while (std::next_permutation(rotation_system[v3->index()].begin(), rotation_system[v3->index()].end()));
-        } while (std::next_permutation(rotation_system[v2->index()].begin(), rotation_system[v2->index()].end()));
-
-    } while (std::next_permutation(rotation_system[v1->index()].begin(), rotation_system[v1->index()].end()));
-    std::cout << "Total embedding: " << embedding << std::endl;
-    return 0;
+    EnumerateEmbedding enumerator(graph, [&]() {
+        std::cout << "Graph's genus = " << graph.genus() << std::endl;
+    });
+    std::cout << enumerator.enumerate() << std::endl;
 }
